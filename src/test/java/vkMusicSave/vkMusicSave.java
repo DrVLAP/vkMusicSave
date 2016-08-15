9f2a8f5b5ae8c1193c7e4361e53f173f721b97c3
First <dr.vlap@gmail.com>
2016-08-15 22:30:16 +0700

    1

--- src/test/java/vkMusicSave/vkMusicSave.java
+++ src/test/java/vkMusicSave/vkMusicSave.java
@@ -1,12 +1,15 @@
 package vkMusicSave;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -22,19 +25,23 @@ public class vkMusicSave {
     public ArrayList<String> musicList = new ArrayList<String>();
     public Integer musicElementIndex;
 
+
     @BeforeMethod
     public void setUp(){
         driver = new FirefoxDriver();
+
     }
 
     @Test
-    public void  musicSave() throws InterruptedException {
+    public void  musicSave() throws InterruptedException, IOException {
         System.out.println("Process started");
         openPage();
         //authLoginAndPassword.setAuthLoginAndPath();
         authorization();
         System.out.println("Authorized;");
+
         musicTabLogick();
+        scrollingToTheEnd();
         makeMusicList();
     }
 
@@ -61,12 +68,12 @@ public class vkMusicSave {
         driver.findElement(By.id("l_aud")).click();
         System.out.println("Music tab opened;");
         Thread.sleep(1500);
-        musicElementList = driver.findElements(By.cssSelector("*[class^='audio_row']"));
+        //musicElementList = driver.findElements(By.cssSelector("*[class^='audio_row']"));
     }
 
-    public void makeMusicList() {
+    public void makeMusicList() throws IOException {
         for (int m = 1; m < musicElementList.size(); m++) {
-            String[] musicData = musicElementList.get(m).getAttribute("data-audio").split(",");
+            String[] musicData = musicElementList.get(m).getAttribute("data-audio").split("\",");
 //            System.out.println("Исполнитель: " + musicData[4]);
 //            System.out.println("Композиция: " + musicData[3]);
 //            System.out.println("------------------------------");
@@ -76,11 +83,41 @@ public class vkMusicSave {
         }
         System.out.println("----------------------------------");
         System.out.println("Список композиций:");
+        FileWriter writer = new FileWriter("C:\\vkmusic.txt", true);
         for (String compositionId: musicList){
 
-            System.out.println(compositionId.replaceAll("&#039;", "'"));
+            System.out.println(compositionId.replaceAll("&#039;", "'").replaceAll("&amp;","&").replaceAll("&quot;","\""));
+            writer.write(compositionId.replaceAll("&#039;", "'").replaceAll("&amp;","&").replaceAll("&quot;","\""));
+        }
+    }
+    //TODO scrolling music method
+    public void scrollingToTheEnd() throws InterruptedException {
+        System.out.println("Start scrolling;");
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        List<WebElement> tempMusicElementList = null;
+        int c = 1;
+        for (int i = 0; i<c; i++) {
+            musicElementList = driver.findElements(By.cssSelector("*[class^='audio_row']"));
+            jse.executeScript("scroll(0, "+(c*2250)+");");
+            System.out.println(c*2250);
+            Thread.sleep(5000);
+            tempMusicElementList = driver.findElements(By.cssSelector("*[class^='audio_row']"));
+            System.out.println("Размер musicElementList = "+musicElementList.size());
+            System.out.println("Размер tempMusicElementList = "+tempMusicElementList.size());
+            System.out.println("-----------------------------");
+            if (musicElementList.size() < tempMusicElementList.size()){
+                c++;
+            }
+            else
+            {
+                System.out.println("Reached end of music list;");
+                return;
+            }
         }
     }
 
+    //TODO music saving method
+    public void musicSaving(){
 
+    }
 }
